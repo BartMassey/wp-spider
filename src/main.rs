cf1c32d13961188ae0aa4b3d0be437f652d06bb9
Bart Massey <bart.massey@gmail.com>
2025-02-13 22:07:17 -0800

    fixed termination conditions

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ const WIKIPEDIA_KEYFILE: &str = ".wikimedia-api-key";
 
 const NWORKERS: usize = 20;
 
-const MAX_DEPTH: usize = 2;
+const MAX_DEPTH: usize = 3;
 
 // Requests per second.
 const RATE_LIMIT: f32 = 1.0;
@@ -37,7 +37,7 @@ fn get_wikimedia_info() -> Option<(String, String)> {
     None
 }
 
-type EdgeSender = mpsc::Sender<(usize, String, String)>;
+type EdgeSender = mpsc::Sender<Option<(usize, String, String)>>;
 
 struct State {
     tp: ThreadPool,
@@ -70,49 +70,63 @@ impl State {
                 .unwrap()
                 .map(|l| l.title);
             for l in links {
-                tx.send((depth + 1, title.clone(), l)).unwrap();
+                tx.send(Some((depth + 1, title.clone(), l))).unwrap();
             }
+            tx.send(None).unwrap();
         });
     }
 }
 
 fn main() {
+    let root = "Rust (programming language)".into();
     let mut s = BTreeMap::new();
     let (tx, rx) = mpsc::channel();
     let state = State::new(tx);
-    state.step(0, "Rust (programming language)".into());
+    let mut outstanding = 1;
+    state.step(0, root);
     let mut nreqs = 1;
 
-    while let Ok((depth, page_title, link_title)) = rx.recv() {
-        if depth >= MAX_DEPTH || s.contains_key(&link_title) {
-            continue;
+    while let Ok(r) = rx.recv() {
+        match r {
+            Some((depth, page_title, link_title)) => {
+                if depth >= MAX_DEPTH || s.contains_key(&link_title) {
+                    continue;
+                }
+
+                eprintln!("{} → {}", page_title, link_title);
+
+                s.entry(page_title)
+                    .and_modify(|v: &mut BTreeSet<String>| {
+                        v.insert(link_title.clone());
+                    })
+                    .or_insert_with(|| {
+                        let mut vs = BTreeSet::new();
+                        vs.insert(link_title.clone());
+                        vs
+                    });
+
+                nreqs += 1;
+                let page_no = nreqs as f32;
+                let secs = state.st.elapsed().as_secs_f32();
+                if page_no > secs * RATE_LIMIT {
+                    let st = page_no / RATE_LIMIT - secs;
+                    eprintln!("{}s sleep", st);
+                    thread::sleep(time::Duration::from_secs_f32(st));
+                }
+
+                outstanding += 1;
+                state.step(depth, link_title);
+
+                let secs = state.st.elapsed().as_secs_f32();
+                eprintln!("{} rps", nreqs as f32 / secs);
+            }
+            None => {
+                outstanding -= 1;
+                if outstanding == 0 {
+                    break;
+                }
+            }
         }
-
-        eprintln!("{} → {}", page_title, link_title);
-
-        s.entry(page_title)
-            .and_modify(|v: &mut BTreeSet<String>| {
-                v.insert(link_title.clone());
-            })
-            .or_insert_with(|| {
-                let mut vs = BTreeSet::new();
-                vs.insert(link_title.clone());
-                vs
-            });
-
-        nreqs += 1;
-        let page_no = nreqs as f32;
-        let secs = state.st.elapsed().as_secs_f32();
-        if page_no > secs * RATE_LIMIT {
-            let st = page_no / RATE_LIMIT - secs;
-            eprintln!("{}s sleep", st);
-            thread::sleep(time::Duration::from_secs_f32(st));
-        }
-
-        state.step(depth, link_title);
-
-        let secs = state.st.elapsed().as_secs_f32();
-        eprintln!("{} rps", nreqs as f32 / secs);
     }
 
     for (k, vs) in s {
